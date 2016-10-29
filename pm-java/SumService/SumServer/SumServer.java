604072c48c97e3d5d45173afa0b0a981aaaf9adb
Rafał Pocztarski <r.pocztarski@gmail.com>
2016-10-29 03:30:18 +0200

    add sumserver

---
+++ pm-java/SumService/SumServer/SumServer.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.net.*;
+
+public class SumServer {
+  public void listen(int port) {
+    ServerSocket server = null;
+    Socket client = null;
+    try {
+      server = new ServerSocket(port);
+    }
+    catch (IOException e) {
+      System.out.println("SumServer cannot listen on port " + port);
+      System.exit(-1);
+    }
+    System.out.println("SumServer is listening on port " + server.getLocalPort());
+
+    while(true) {
+      try {
+        client = server.accept();
+      }
+      catch (IOException e) {
+        System.out.println("Accept failed");
+        System.exit(-1);
+      }
+      (new SumServerThread(client)).start();
+    }
+
+  }
+
+  public static void main(String[] args) {
+    new SumServer().listen(args.length > 0 ? Integer.parseInt(args[0]) : 12345);
+  }
+}
---
+++ pm-java/SumService/SumServer/SumServerThread.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import java.net.*;
+import java.util.Arrays;
+
+public class SumServerThread extends Thread {
+
+  private Socket socket;
+
+  public SumServerThread(Socket socket) {
+    super();
+    this.socket = socket;
+  }
+
+  String handler(String req) {
+    String[] args = req.trim().split("\\s+");
+    int a = args.length > 0 ? Integer.parseInt(args[0]) : 0;
+    int b = args.length > 1 ? Integer.parseInt(args[1]) : 0;
+    return "" + (a + b);
+  }
+
+  public void run() {
+    try {
+      BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+      PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+      String address = socket.getInetAddress().getHostAddress();
+      int port = socket.getPort();
+      String remote = "I " + address + ":" + port;
+      System.out.println(remote + " : connected");
+      String req = in.readLine();
+      System.out.println(remote + " > " + req);
+      String res = handler(req);
+      System.out.println(remote + " < " + res);
+      out.println(res);
+      socket.close();
+    } catch (IOException e) {
+      System.out.println("IOException!");
+    }
+  }
+
+}
